4d200f9e8d74d53f0ee4283cd456382d4e5f5c59
Alaa Eddin Albarghoth <alaaeddin.albarghoth@appcent.mobi>
2021-12-29 13:53:06 +0300

    :shirt: Another Some cleanup

--- app/src/main/java/com/alaaeddinalbarghoth/satellites/utils/navigation/NavigationService.kt
+++ app/src/main/java/com/alaaeddinalbarghoth/satellites/utils/navigation/NavigationService.kt
@@ -1,14 +1,7 @@
 package com.alaaeddinalbarghoth.satellites.utils.navigation
 
-import android.os.Bundle
-
 /** A Service that control navigation between the activities components in the app
  * Also it simplifies the control the navigation from the business logic of the ui */
 interface NavigationService {
-
-    fun finishActivity()
-
     fun navigateToActivity(screen: Screen, finishCurrent: Boolean = true)
-
-    fun navigateToActivity(screen: Screen, bundle: Bundle, finishCurrent: Boolean = true)
 }
